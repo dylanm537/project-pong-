09aca6a77c05b081faad4b76d9239945f6be00a5
DylanM-2 <Donaldjaminson@yahoo.com>
2025-09-15 16:37:17 +0100

    Adding changes to the main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.cpp>
+
+void init() {
+	// initialise all the objects needed for the game. 
+}
+
+void update(float dt) {
+	// Update Everything
+}
+
+void render(sf::RenderWindow& window) {
+	// Draw Everything
+}
+
+void clean() {
+	//free up the memory if necessary.
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-    return 0;
+	//create the window
+	sf::RenderWindow window(sf::VideoMode({ game_width, game_height }), "PONG");
+	//initialise and load
+	init();
+	while (!window.isOpen()) {
+		//Calculate dt
+		...
+			window.clear();
+		update(dt);
+		render(window);
+		//wait for the time_step to finish before displaying the next frame.
+		sf::sleep(time_step);
+		//Wait for Vsync
+		window.display()
+	}
+	//Unload and shutdown
+	clean();
 }
